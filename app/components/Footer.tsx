a998f9d948cc1208ded50b84e33c201b5e670a82
jayzumen <reinhardtjanniklas96@gmail.com>
2023-02-19 15:19:17 +0100

    initial commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        This product uses the TMDB API but is not endorsed or certified by TMDB.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className="min-h-screen bg-slate-900 text-white">
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
