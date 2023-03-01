62b9d0c79ceb64883772e9c35c56acbb60d14104
jayzumen <reinhardtjanniklas96@gmail.com>
2023-03-01 11:05:54 +0100

    liked movies displayed

--- app/account/AccountDetails.tsx
+++ app/account/AccountDetails.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { auth } from "@/utils/firebase";
+import { auth, db } from "@/utils/firebase";
 import { onAuthStateChanged, User } from "firebase/auth";
+import { collection, getDocs } from "firebase/firestore";
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { baseUrl } from "../constants";
 import { Movie } from "../movies/movieTypes";
 
 const AccountDetails = () => {
@@ -22,9 +26,40 @@ const AccountDetails = () => {
     getUser();
   }, []);
 
+  useEffect(() => {
+    if (!user) return;
+    const getMovies = async (user: User) => {
+      const colRef = collection(db, "users", user?.uid, "likedMovies");
+      const snapshot = await getDocs(colRef);
+      const movies: Movie[] = [];
+      snapshot.forEach((doc) => {
+        movies.push(doc.data() as Movie);
+      });
+      setMovies(movies);
+    };
+    getMovies(user);
+  }, [user]);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold">{user?.displayName}' page</h1>
+    <div className="">
+      <h1 className="mb-8 text-3xl font-bold">{user?.displayName}' page</h1>
+      <div className="flex flex-wrap gap-8">
+        {movies.map((movie) => (
+          <Link
+            aria-label={`Link to ${movie.title} page`}
+            key={movie.id}
+            href={`/movies/${movie.id}`}
+          >
+            <Image
+              className="rounded-md object-cover"
+              height={600}
+              width={400}
+              src={baseUrl + movie.poster_path || movie.backdrop_path}
+              alt={movie.title || movie.original_title}
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
