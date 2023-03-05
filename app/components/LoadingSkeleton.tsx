52b4f131fd2d5f76d05bb8c7b030e8c80c094ab4
jayzumen <reinhardtjanniklas96@gmail.com>
2023-03-05 17:06:09 +0100

    added loading pages

---
+++ app/components/LoadingSkeleton.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+import { FaSpinner } from "react-icons/fa";
+
+const LoadingSkeleton = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="animate-spin text-white">
+        <FaSpinner className="text-6xl" />
+      </div>
+      <h1 className="text-2xl font-semibold">Loading...</h1>
+    </div>
+  );
+};
+
+export default LoadingSkeleton;
